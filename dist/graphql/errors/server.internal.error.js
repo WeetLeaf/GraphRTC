bf7f990148208e660c3c23f97ade1ff4eb08b2b4
Martin PELCAT <martinpelcat@hotmail.com>
2022-04-20 17:45:18 +0200

    rm dist folder

--- dist/graphql/errors/server.internal.error.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ServerInternalError = void 0;
-class ServerInternalError extends Error {
-    constructor() {
-        super('Server internal error');
-    }
-}
-exports.ServerInternalError = ServerInternalError;
