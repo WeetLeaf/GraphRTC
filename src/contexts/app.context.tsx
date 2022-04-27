c5a8e5a9d7605ffe616691d17b445cdb7df70a81
pelcatmart <martinpelcat@hotmail.com>
2022-04-27 15:04:56 +0200

    useless shit

--- src/contexts/app.context.tsx
+++ src/contexts/app.context.tsx
@@ -1,14 +1,9 @@
 import { ApolloProvider } from "@apollo/client";
 import { ChakraProvider } from "@chakra-ui/react";
-import { useRouter } from "next/router";
 import { PropsWithChildren } from "react";
 import { apolloClient } from "../clients/apollo.client";
 
 export default function AppContext({ children }: PropsWithChildren<{}>) {
-  const router = useRouter();
-
-  console.log(router);
-
   return (
     <ApolloProvider client={apolloClient}>
       <ChakraProvider>{children}</ChakraProvider>
